9c659fb3b538cf100cb9492f9edd2cd14c5cf3cf
fourside <fourside@gmail.com>
2012-01-01 22:38:20 +0900

    fix timing to connect

--- link.rb
+++ link.rb
@@ -1,10 +1,12 @@
 require 'rubygems'
 require 'active_record'
+require 'logger'
+
+ActiveRecord::Base.establish_connection(
+	:adapter => 'sqlite3',
+	:database => File.join(File.dirname(__FILE__), '/db/links.db')
+	)
+ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), '/database.log'))
 
 class Link < ActiveRecord::Base
-	ActiveRecord::Base.establish_connection(
-		:adapter => 'sqlite3',
-		:database => File.join(File.dirname(__FILE__), '/db/links.db')
-		)
-	ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), '/database.log'))
 end
