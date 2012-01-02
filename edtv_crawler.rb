611c9e5afd26bbbd44a8b737c85e0df2ed553dce
fourside <fourside@gmail.com>
2012-01-02 11:39:00 +0900

    refactor variable

--- edtv_crawler.rb
+++ edtv_crawler.rb
@@ -23,23 +23,24 @@ class EdtvCrawler
 		@agent.page.search("//small[@id='trad']/a").each do |elem|
 			if elem.inner_text =~ /NHK教育を見て/
 				url = BASEURL + elem['href'].gsub(/l50/, '')
-				links[url] = elem.inner_text
+				title = elem.inner_text.gsub(/^\d+:/, '').gsub(/\(\d+\)/, '').strip
+				links[url] = title
 			end
 		end
 		links
 	end
 
 # in bbs thread
-	def scrapelinks(url)
-		@agent.get(url)
+	def scrapelinks(thread_url)
+		@agent.get(thread_url)
 		@agent.page.encoding = 'CP932'
 		list = []
 		@agent.page.search("//dd").each do |elem|
 			if elem.inner_text.index('ttp')
-				elem.inner_text.split.each do |s|
-					next unless /^h?ttp:\/\/(?:www\.)?(?:iup|10up|epcan|jlab|tv|uproda|rupan|ruru2).*\.(?:jpg|gif|png|jpeg)$/ =~ s
-					s = 'h' + s unless s[0] == 'h'
-					list << s
+				elem.inner_text.split.each do |url|
+					next unless /^h?ttp:\/\/(?:www\.)?(?:iup|10up|epcan|jlab|tv|uproda|rupan|ruru2).*\.(?:jpg|gif|png|jpeg)$/ =~ url
+					url = 'h' + url unless url[0] == 'h'
+					list << url
 				end
 			end
 		end
@@ -60,3 +61,6 @@ class EdtvCrawler
 	end
 end
 
+if __FILE__ == $0
+	EdtvCrawler.new.save_links
+end
