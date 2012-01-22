869d0fa71ba6076048e963d0fbc5e0eb29762d75
fourside <fourside@gmail.com>
2012-01-22 16:29:51 +0900

    add config for heroku, and add validation of unique url

--- link.rb
+++ link.rb
@@ -1,15 +1,30 @@
 require 'rubygems'
 require 'active_record'
 require 'logger'
+require 'yaml'
+require 'uri'
 
-ActiveRecord::Base.establish_connection(
-	:adapter => 'sqlite3',
-	:database => File.join(File.dirname(__FILE__), '/db/links.db')
+if ENV['DATABASE_URL']
+	db = URI.parse(ENV['DATABASE_URL'])
+	ActiveRecord::Base.establish_connection(
+		:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+		:host     => db.host,
+		:username => db.user,
+		:password => db.password,
+		:database => db.path[1..-1],
+		:encoding => 'utf8',
+		:min_messages => 'notice'
 	)
-ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), '/database.log'))
+else
+	dbconfig = YAML.load_file(File.dirname(__FILE__) + '/config/database.yml')
+	ActiveRecord::Base.establish_connection(dbconfig)
+end
+ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/log/database.log')
 
 class Link < ActiveRecord::Base
+	validates_uniqueness_of :image_url
 	def caption
 		"<a href='#{self.thread_url}'>#{self.title}</a>"
 	end
 end
+
