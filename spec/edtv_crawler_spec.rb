688819a39df304297847658cf0300063e304e63d
fourside <fourside@gmail.com>
2012-01-22 16:31:52 +0900

    add spec for subbacks.yaml

--- spec/edtv_crawler_spec.rb
+++ spec/edtv_crawler_spec.rb
@@ -2,15 +2,19 @@
 require 'rspec'
 require '../edtv_crawler'
 require '../db/001_create_links'
+require '../db/002_add_column_links'
 
 describe EdtvCrawler do
-	before do
+	before(:all) do
 		@crawler = EdtvCrawler.new
+		@subback = {
+			'url' => "http://hayabusa2.2ch.net/livenhk/subback.html",
+			'tv' => 'ntv'}
 	end
 
 	describe "when call thread_urls method" do
 		before do
-			@thread_urls = @crawler.thread_urls
+			@thread_urls = @crawler.thread_urls(@subback['url'])
 		end
 
 		it "should get hash" do
@@ -23,16 +27,16 @@ describe EdtvCrawler do
 			end
 		end
 
-		it "should get hash that value is title" do
+		it "should get hash that value is title, which starts no number" do
 			@thread_urls.values.each do |v|
-				v.should =~ /NHK教育を見て/
+				v.should =~ /^[^\d]+/
 			end
 		end
 	end
 
 	describe "when call scrapelinks method" do
 		before do
-			urls = @crawler.thread_urls.keys
+			urls = @crawler.thread_urls(@subback['url']).keys
 			@scrapelinks = @crawler.scrapelinks(urls[1])
 		end
 
@@ -55,10 +59,29 @@ describe EdtvCrawler do
 				:database => ':memory:'
 			)
 			CreateLinks::up
+			AddColumnLinks::up
 		end
 		it "should saved image link" do
-			@crawler.save_links
+			@crawler.save_links(@subback)
 			Link.find(:all).size.should have_at_least(1).items
+		end
+	end
+
+	describe "when call subbacks" do
+		before do
+			@subbacks = @crawler.subbacks
+		end
+		it "should return array of subback" do
+			@subbacks.class.should be Array
+		end
+		it "should return array of subback, which element is hash" do
+			@subbacks.first.class.should be Hash
+		end
+		it "should return array of subback, which hash has url" do
+			@subbacks.first['url'].should match /^http:/
+		end
+		it "should return array of subback, which hash has tv name" do
+			@subbacks.first['tv'].should match /^(etv|nhk|bs|mx)$/
 		end
 	end
 
