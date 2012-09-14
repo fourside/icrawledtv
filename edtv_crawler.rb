478810f1576cd0b10f7e777266111b84e6e56d32
fourside <fourside@gmail.com>
2012-09-14 12:19:09 +0900

    refactor about handling image url

--- edtv_crawler.rb
+++ edtv_crawler.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'mechanize'
 require 'open-uri'
+require 'uri'
 require 'RMagick'
 # TODO
 # thread
@@ -33,7 +34,7 @@ class EdtvCrawler
           local_file = download_img(img_url)
           next unless local_file
           raise "file size is zero: #{local_file}" if File.size(local_file) == 0
-          thumbnail = scale_img(local_file)
+          thumbnail = make_thumbnail(local_file)
           link.image_url, link.thread_url, link.title, link.tv      =
           img_url,        thread_url,      title,      subback_name
           link.save
@@ -66,21 +67,26 @@ class EdtvCrawler
     list = []
     @agent.page.search("//dd").each do |elem|
       if elem.inner_text.index('ttp')
-        elem.inner_text.split(/ |　/).each do |url|
-          #next unless /ttp:\/\/(?:www\.)?(?:dotup|iup|10up|epcan|jlab|tv|uproda|rupan|ruru2|tv2ch|motto-jimidane|file\.jabro|hayabusa|folderman|live2|age2|pa4\.dip\.jp).*\.(?:jpg|gif|png|jpeg)$/i =~ url
-          unless /ttp:\/\/(?:www\.)?(?:dotup|iup|10up|epcan|jlab|tv|uproda|rupan|ruru2|tv2ch|motto-jimidane|file\.jabro|hayabusa|folderman|live2|age2|pa4?\.dip\.jp|up2?\.pandoravote\.net|long\.2chan\.tv|fat\.5pb\.org|up\.null-x|cap\d{3}\.areya|tv\.dee|fastpic\.jp|livetests\.info|katsakuri\.sakura).*\.(?:jpg|gif|png|jpeg)$/i =~ url
-            File.open("except_url.txt", "a") {|file| file.write(url + "\n") } if url.index('ttp')
-            next
+        elem.inner_text.split(/ |　/).each do |url_text|
+          uri = URI.parse(url_text)
+          uri.scheme = 'http' unless uri.scheme != 'http'
+          if uploader? uri
+            list << uri.to_s
+          else
+            File.open("except_url.txt", "a") {|file| file.write(uri.to_s + "\n") } if uri.path.index(/jpe?g|png|gif/)
           end
-          url = 'h' + url unless url[0] == 'h'
-          list << url
         end
       end
     end
     list
   end
 
-  def download_img url, path_prefix = File.dirname(__FILE__) + '/public/img/'
+  def uploader? url
+    /ttp:\/\/(?:www\.)?(?:dotup|iup|10up|epcan|jlab|tv|uproda|rupan|ruru2|tv2ch|motto-jimidane|file\.jabro|hayabusa|folderman|live2|age2|pa4?\.dip\.jp|up2?\.pandoravote\.net|long\.2chan\.tv|fat\.5pb\.org|up\.null-x|cap\d{3}\.areya|tv\.dee|fastpic\.jp|livetests\.info|katsakuri\.sakura).*\.(?:jpg|gif|png|jpeg)$/i =~ url
+  end
+
+  def download_img url
+    path_prefix = File.dirname(__FILE__) + '/public/img/'
     filename = path_prefix + File.basename(url)
     return nil if File.exist?(path_prefix + filename)
     open(filename, 'wb') do |file|
@@ -91,7 +97,7 @@ class EdtvCrawler
     filename
   end
 
-  def scale_img file, scale = 0.5
+  def make_thumbnail file, scale = 0.5
     thumnail = Magick::Image.read(file).first.scale(scale)
     file_prefix = 'thumbnail_'
     output_dir  = File.dirname(__FILE__) + '/public/img/thumbnail/'
